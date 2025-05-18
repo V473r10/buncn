153accaa608507a2ea1250a0704b86d87d548f04
v473r10 <fac.valerio@gmail.com>
2025-05-18 14:25:06 -0300

    Move packages to packages folder

--- ui-bak/app/routes.ts
+++
@@ -1,3 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [index("routes/home/home.tsx"), route("auth/sign-in", "routes/auth/sign-in/sign-in.tsx")] satisfies RouteConfig;
