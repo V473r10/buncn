a094d81b12721c1ba3768774e0f68cef27df386e
v473r10 <fac.valerio@gmail.com>
2025-05-21 00:56:08 -0300

    refactor: migrate from createTRPCProxyClient to createTRPCClient

--- packages/ui/src/lib/trpc-client.ts
+++ packages/ui/src/lib/trpc-client.ts
@@ -1,11 +1,8 @@
-import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
+import { createTRPCClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "api/src/routes/_app";
 
-export const trpcClient = createTRPCProxyClient<AppRouter>({
+export const trpcClient = createTRPCClient<AppRouter>({
 	links: [
-		// httpLink({
-		//   url: "http://localhost:4001/trpc",
-		// }),
 		httpBatchLink({
 			url: "http://localhost:3000/api/trpc",
 			// You can pass any HTTP headers you wish here
